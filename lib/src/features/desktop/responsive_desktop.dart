04047575362ed8f5a18c90d0b59548188b61893f
Project Zion <project-zion@github.com>
2026-06-10 22:01:58 +0300

    🆘 Add Help & Support Center with articles, FAQs, support tickets, quick actions, documentation links

--- lib/src/features/desktop/responsive_desktop.dart
+++ lib/src/features/desktop/responsive_desktop.dart
@@ -31,6 +31,8 @@ import '../backup_restore/backup_restore_center.dart';
 import '../monitoring/monitoring_center.dart';
 import '../games/games_center.dart';
 import '../learning/learning_center.dart';
+import '../store/app_store.dart';
+import '../help/help_center.dart';
 import '../downloads/download_manager.dart';
 import '../network_control/network_control_center.dart';
 import '../maintenance/maintenance_center.dart';
@@ -38,6 +40,8 @@ import '../backup_restore/backup_restore_center.dart';
 import '../monitoring/monitoring_center.dart';
 import '../games/games_center.dart';
 import '../learning/learning_center.dart';
+import '../store/app_store.dart';
+import '../help/help_center.dart';
 import '../downloads/download_manager.dart';
 import '../network_control/network_control_center.dart';
 import '../maintenance/maintenance_center.dart';
@@ -45,6 +49,8 @@ import '../backup_restore/backup_restore_center.dart';
 import '../monitoring/monitoring_center.dart';
 import '../games/games_center.dart';
 import '../learning/learning_center.dart';
+import '../store/app_store.dart';
+import '../help/help_center.dart';
 import '../security_center/security_center.dart';
 import '../tasks/advanced_task_manager.dart';
 import '../downloads/download_manager.dart';
@@ -54,6 +60,8 @@ import '../backup_restore/backup_restore_center.dart';
 import '../monitoring/monitoring_center.dart';
 import '../games/games_center.dart';
 import '../learning/learning_center.dart';
+import '../store/app_store.dart';
+import '../help/help_center.dart';
 import '../downloads/download_manager.dart';
 import '../network_control/network_control_center.dart';
 import '../maintenance/maintenance_center.dart';
@@ -61,6 +69,8 @@ import '../backup_restore/backup_restore_center.dart';
 import '../monitoring/monitoring_center.dart';
 import '../games/games_center.dart';
 import '../learning/learning_center.dart';
+import '../store/app_store.dart';
+import '../help/help_center.dart';
 import '../downloads/download_manager.dart';
 import '../network_control/network_control_center.dart';
 import '../maintenance/maintenance_center.dart';
@@ -68,6 +78,8 @@ import '../backup_restore/backup_restore_center.dart';
 import '../monitoring/monitoring_center.dart';
 import '../games/games_center.dart';
 import '../learning/learning_center.dart';
+import '../store/app_store.dart';
+import '../help/help_center.dart';
 import '../notifications/advanced_notification_center.dart';
 import '../../../cosmic_terminal.dart';
 
@@ -627,3 +639,9 @@ IconButton(
 
 // إضافة في قائمة icons في _buildDesktopIcons
 {'icon': Icons.school, 'label': 'Learning', 'widget': const LearningCenter(), 'color': Colors.teal},
+
+// إضافة في قائمة icons في _buildDesktopIcons
+{'icon': Icons.store, 'label': 'App Store', 'widget': const AppStore(), 'color': Colors.blue},
+
+// إضافة في قائمة icons في _buildDesktopIcons
+{'icon': Icons.help, 'label': 'Help', 'widget': const HelpCenter(), 'color': Colors.blue},
