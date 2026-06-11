86809303905ace439c35adebc80dda4f9d5279ce
Project Zion <project-zion@github.com>
2026-06-11 03:46:10 +0300

    Extreme minimal test

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const ZionOS());
-}
-
-class ZionOS extends StatelessWidget {
-  const ZionOS({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Zion OS',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Colors.black,
-        primaryColor: Colors.green,
-      ),
-      home: const Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.security, color: Colors.green, size: 80),
-              SizedBox(height: 20),
-              Text(
-                'ZION OS v3.3',
-                style: TextStyle(color: Colors.green, fontSize: 32, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Loading...',
-                style: TextStyle(color: Colors.white70),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(Container());
 }
